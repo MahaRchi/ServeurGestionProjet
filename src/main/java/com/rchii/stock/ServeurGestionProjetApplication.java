bdf313bf55dbd0d0dd5f46e15c1e5af4ac8d80a6
Maha <mahrchi93@gmail.com>
2018-12-30 09:29:35 +0100

    first commit

---
+++ src/main/java/com/rchii/stock/ServeurGestionProjetApplication.java
@@ -0,0 +1,14 @@
+package com.rchii.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionProjetApplication.class, args);
+	}
+
+}
+
